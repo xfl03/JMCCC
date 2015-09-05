167e2c2c2ac18e74c2a8f6a610c3ae061c63fe0b
yushijinhun <yushijinhun@gmail.com>
2015-09-05 20:37:13 +0800

    change version form '2.0-dev' to '2.0'

--- src/main/java/com/github/to2mbn/jmccc/util/References.java
+++ src/main/java/com/github/to2mbn/jmccc/util/References.java
@@ -3,7 +3,7 @@ package com.github.to2mbn.jmccc.util;
 public final class References {
 
     public static final String ID = "jmccc";
-    public static final String VERSION = "2.0-dev";
+    public static final String VERSION = "2.0";
 
     private References() {
     }
