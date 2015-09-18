d31e8ef6b7b80a3229ea1f3b5108a829a19fae95
yushijinhun <yushijinhun@gmail.com>
2015-09-18 17:46:09 +0800

    version bump to 2.1.1

--- src/main/java/com/github/to2mbn/jmccc/util/References.java
+++ src/main/java/com/github/to2mbn/jmccc/util/References.java
@@ -3,7 +3,7 @@ package com.github.to2mbn.jmccc.util;
 public final class References {
 
     public static final String ID = "jmccc";
-    public static final String VERSION = "2.1";
+    public static final String VERSION = "2.1.1";
 
     private References() {
     }
