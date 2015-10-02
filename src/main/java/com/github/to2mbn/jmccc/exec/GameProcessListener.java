2c19f5e84d8672ecb4222868083328878dd41383
yushijinhun <yushijinhun@gmail.com>
2015-10-02 22:28:47 +0800

    add javadoc for GameProcessListener

--- src/main/java/com/github/to2mbn/jmccc/exec/GameProcessListener.java
+++ src/main/java/com/github/to2mbn/jmccc/exec/GameProcessListener.java
@@ -1,7 +1,25 @@
 package com.github.to2mbn.jmccc.exec;
 
 public interface GameProcessListener {
+
+    /**
+     * Calls when recieves a log from stdout.
+     * 
+     * @param log the log
+     */
     void onLog(String log);
+
+    /**
+     * Calls when recieves a log from stderr
+     * 
+     * @param log the log
+     */
     void onErrorLog(String log);
+
+    /**
+     * Calls when the game process exits.
+     * 
+     * @param code the exit code of the game process
+     */
     void onExit(int code);
 }
