c4b062be946da5c08447b6f892b16a9aa2ab90f4
yushijinhun <yushijinhun@gmail.com>
2015-10-24 18:23:43 +0800

    update MinecraftDownloader interface

--- src/main/java/com/github/to2mbn/jmccc/mcdownloader/MinecraftDownloader.java
+++ src/main/java/com/github/to2mbn/jmccc/mcdownloader/MinecraftDownloader.java
@@ -1,111 +1,61 @@
 package com.github.to2mbn.jmccc.mcdownloader;
 
-import java.util.concurrent.Future;
-import com.github.to2mbn.jmccc.mcdownloader.download.DownloadGroupFutureCallback;
+import com.github.to2mbn.jmccc.mcdownloader.download.DownloadTask;
 import com.github.to2mbn.jmccc.option.MinecraftDirectory;
 import com.github.to2mbn.jmccc.version.Library;
 
 public interface MinecraftDownloader {
 
 	/**
-	 * Downloads the version list asynchronously.
-	 * <p>
-	 * This method downloads the version list to memory.
-	 * <p>
-	 * Exceptions:
+	 * Returns a version list download task.
 	 * 
-	 * <pre>
-	 * IOException - if an i/o error occurs
-	 * JSONException - if fails to resolve json
-	 * </pre>
-	 * 
-	 * @param callback download callback
-	 * @return future representing pending completion of the download
+	 * @return a version list download task
 	 */
-	Future<RemoteVersionList> downloadVersionList(DownloadGroupFutureCallback<RemoteVersionList> callback);
+	DownloadTask<RemoteVersionList> versionList();
 
 	/**
-	 * Downloads the asset index asynchronously.
-	 * <p>
-	 * If <code>useCache==true</code> and <code>${mcdir}/assets/indexes/${version}</code> exists, this method will use
-	 * the cached asset index. Else this method will download the asset index and caches it. If a json exception occurs
-	 * during handling the cached asset index, this method will download the asset index again.
-	 * <p>
-	 * Exceptions:
-	 * 
-	 * <pre>
-	 * IOException - if an i/o error occurs
-	 * JSONException - if fails to resolve json
-	 * </pre>
+	 * Returns an asset index download task.
 	 * 
 	 * @param mcdir the minecraft dir
 	 * @param version the asset index version
-	 * @param useCache true if uses the cached asset index
-	 * @param callback download callback
-	 * @return future representing pending completion of the download
+	 * @return an asset index download task
 	 */
-	Future<AssetsIndex> downloadAssetsIndex(MinecraftDirectory mcdir, String version, boolean useCache, DownloadGroupFutureCallback<AssetsIndex> callback);
+	DownloadTask<AssetsIndex> assetsIndex(MinecraftDirectory mcdir, String version);
 
 	/**
-	 * Downloads the game jar asynchronously.
+	 * Returns a game jar download task.
 	 * <p>
 	 * The jar is saved to <code>${mcdir}/versions/${version}/${version}.jar</code>. If the file already exists, this
 	 * method will overwrite the file.
-	 * <p>
-	 * Exceptions:
-	 * 
-	 * <pre>
-	 * IOException - if an i/o error occurs
-	 * JSONException - if fails to resolve json
-	 * </pre>
 	 * 
 	 * @param mcdir the minecraft dir
 	 * @param version the game version
-	 * @param callback download callback
-	 * @return future representing pending completion of the download
+	 * @return a game jar download task
 	 */
-	Future<?> downloadGameJar(MinecraftDirectory mcdir, String version, DownloadGroupFutureCallback<?> callback);
+	DownloadTask<?> gameJar(MinecraftDirectory mcdir, String version);
 
 	/**
-	 * Downloads the library asynchronously.
+	 * Returns a library download task.
 	 * <p>
 	 * The library is saved to <code>${mcdir}/libraries/${library.getPath()}</code>. If the file already exists, this
 	 * method will overwrite the file.
-	 * <p>
-	 * Exceptions:
-	 * 
-	 * <pre>
-	 * IOException - if an i/o error occurs
-	 * JSONException - if fails to resolve json
-	 * ChecksumException - if the checksum mismatches
-	 * </pre>
 	 * 
 	 * @param mcdir the minecraft dir
 	 * @param library the library to download
-	 * @param callback download callback
-	 * @return future representing pending completion of the download
+	 * @return a library download task
 	 */
-	Future<?> downloadLibrary(MinecraftDirectory mcdir, Library library, DownloadGroupFutureCallback<?> callback);
+	DownloadTask<?> library(MinecraftDirectory mcdir, Library library);
 
 	/**
-	 * Downloads the asset asynchronously.
+	 * Returns an asset download task.
 	 * <p>
-	 * The library is saved to <code>${mcdir}/libraries/${library.getPath()}</code>. If the file already exists, this
-	 * method will overwrite the file.
-	 * <p>
-	 * Exceptions:
-	 * 
-	 * <pre>
-	 * IOException - if an i/o error occurs
-	 * JSONException - if fails to resolve json
-	 * ChecksumException - if the checksum mismatches
-	 * </pre>
+	 * The asset is saved to <code>${mcdir}/assets/objects/${2-character-prefix of hash}/${hash}</code>. If the file
+	 * already exists, this method will overwrite the file.
 	 * 
 	 * @param mcdir the minecraft dir
 	 * @param asset the asset to download
-	 * @param callback download callback
-	 * @return future representing pending completion of the download
+	 * @return an asset download task
 	 */
-	Future<?> downloadAsset(MinecraftDirectory mcdir, Asset asset, DownloadGroupFutureCallback<?> callback);
+	DownloadTask<?> asset(MinecraftDirectory mcdir, Asset asset);
 
 }
