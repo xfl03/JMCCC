e0e0cc73a9e391891806fbaba91a645732fdfc4e
yushijinhun <yushijinhun@gmail.com>
2015-10-30 21:56:56 +0800

    add MinecraftDownloader

---
+++ src/main/java/com/github/to2mbn/jmccc/mcdownloader/MinecraftDownloader.java
@@ -0,0 +1,25 @@
+package com.github.to2mbn.jmccc.mcdownloader;
+
+import java.util.concurrent.Future;
+import java.util.concurrent.RejectedExecutionException;
+import com.github.to2mbn.jmccc.mcdownloader.download.concurrent.Shutdownable;
+import com.github.to2mbn.jmccc.mcdownloader.download.group.MultipleDownloadCallback;
+import com.github.to2mbn.jmccc.option.MinecraftDirectory;
+
+public interface MinecraftDownloader extends Shutdownable {
+
+	/**
+	 * Downloads a minecraft version incrementally and asynchronously.
+	 * <p>
+	 * This methods checks assets, libraries, game jars. And downloads the missing or broken files.
+	 * 
+	 * @param dir the minecraft dir
+	 * @param version the version to download
+	 * @param callback the callback
+	 * @return future representing pending completion of the operation
+	 * @throws NullPointerException if <code>dir==null || version==null</code>
+	 * @throws RejectedExecutionException if the downloader has been shutdown
+	 */
+	Future<Object> downloadIncrementally(MinecraftDirectory dir, String version, MultipleDownloadCallback<Object> callback);
+
+}
