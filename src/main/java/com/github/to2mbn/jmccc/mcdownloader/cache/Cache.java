f1f12b6d46cfe7b98fd40064d6aed7c88c774554
yushijinhun <yushijinhun@gmail.com>
2015-10-31 16:57:58 +0800

    Revert "add cache interfaces"
    This reverts commit 0a8d7a83cdd7e9c3f439eb7bb036b887f330d38e.

--- src/main/java/com/github/to2mbn/jmccc/mcdownloader/cache/Cache.java
+++
@@ -1,9 +0,0 @@
-package com.github.to2mbn.jmccc.mcdownloader.cache;
-
-public interface Cache<T> {
-
-	T get() throws Exception;
-
-	void update() throws Exception;
-
-}
--- src/main/java/com/github/to2mbn/jmccc/mcdownloader/cache/CacheTable.java
+++
@@ -1,7 +0,0 @@
-package com.github.to2mbn.jmccc.mcdownloader.cache;
-
-public interface CacheTable<K, V> {
-
-	Cache<V> get(K key);
-
-}
