cc7b00f1dcd4d9bcb850eed305a91decdadf8ed4
yushijinhun <yushijinhun@gmail.com>
2015-11-20 19:40:13 +0800

    update MultipleDownloadContext

--- src/main/java/com/github/to2mbn/jmccc/mcdownloader/download/group/MultipleDownloadContext.java
+++ src/main/java/com/github/to2mbn/jmccc/mcdownloader/download/group/MultipleDownloadContext.java
@@ -2,6 +2,7 @@ package com.github.to2mbn.jmccc.mcdownloader.download.group;
 
 import java.util.concurrent.Callable;
 import java.util.concurrent.Future;
+import com.github.to2mbn.jmccc.mcdownloader.download.DownloadCallback;
 import com.github.to2mbn.jmccc.mcdownloader.download.DownloadTask;
 import com.github.to2mbn.jmccc.mcdownloader.download.concurrent.AsyncCallback;
 
@@ -11,6 +12,6 @@ public interface MultipleDownloadContext<T> extends AsyncCallback<T> {
 
 	<R> Future<R> submit(Callable<R> task, AsyncCallback<R> callback, boolean fatal) throws InterruptedException;
 
-	<R> Future<R> submit(DownloadTask<R> task, AsyncCallback<?> callback, boolean fatal) throws InterruptedException;
+	<R> Future<R> submit(DownloadTask<R> task, DownloadCallback<R> callback, boolean fatal) throws InterruptedException;
 
 }
