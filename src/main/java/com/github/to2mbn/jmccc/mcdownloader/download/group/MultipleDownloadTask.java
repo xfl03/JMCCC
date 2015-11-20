9bdc98daf9f21d5f3b11b8e204c40366e4b5f259
yushijinhun <yushijinhun@gmail.com>
2015-11-20 20:08:52 +0800

    add throws for MultipleDownloadTask

--- src/main/java/com/github/to2mbn/jmccc/mcdownloader/download/group/MultipleDownloadTask.java
+++ src/main/java/com/github/to2mbn/jmccc/mcdownloader/download/group/MultipleDownloadTask.java
@@ -2,6 +2,6 @@ package com.github.to2mbn.jmccc.mcdownloader.download.group;
 
 public interface MultipleDownloadTask<T> {
 
-	void execute(MultipleDownloadContext<T> context);
+	void execute(MultipleDownloadContext<T> context) throws Exception;
 
 }
