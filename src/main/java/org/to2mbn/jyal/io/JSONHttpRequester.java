6bca9722fc28ca031a10a648fb6733952cc71c5c
yushijinhun <yushijinhun@gmail.com>
2016-01-31 19:53:03 +0800

    rename package com.github.to2mbn -> org.to2mbn

---
+++ src/main/java/org/to2mbn/jyal/io/JSONHttpRequester.java
@@ -0,0 +1,34 @@
+package org.to2mbn.jyal.io;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.Proxy;
+import java.util.Map;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class JSONHttpRequester extends HttpRequester {
+
+	public JSONHttpRequester() {
+	}
+
+	public JSONHttpRequester(Proxy proxy) {
+		super(proxy);
+	}
+
+	public JSONObject jsonPost(String url, Map<String, Object> arguments, JSONObject post) throws JSONException, UnsupportedEncodingException, MalformedURLException, IOException {
+		return toJson(post(url, arguments, post.toString(), "application/json"));
+	}
+
+	public JSONObject jsonGet(String url, Map<String, Object> arguments) throws JSONException, UnsupportedEncodingException, MalformedURLException, IOException {
+		return toJson(get(url, arguments));
+	}
+
+	private JSONObject toJson(String json) {
+		if (json == null || json.trim().isEmpty()) {
+			return null;
+		}
+		return new JSONObject(json);
+	}
+}
---
+++ src/main/java/org/to2mbn/jyal/yggdrasil/YggdrasilSession.java
@@ -0,0 +1,60 @@
+package org.to2mbn.jyal.yggdrasil;
+
+import java.util.Arrays;
+import java.util.Map;
+import org.to2mbn.jyal.GameProfile;
+import org.to2mbn.jyal.Session;
+import org.to2mbn.jyal.UserType;
+
+public class YggdrasilSession implements Session {
+
+	private String userId;
+	private Map<String, String> properties;
+	private String accessToken;
+	private GameProfile[] profiles;
+	private GameProfile selectedProfile;
+
+	public YggdrasilSession(String userId, Map<String, String> properties, String accessToken, GameProfile[] profiles, GameProfile selectedProfile) {
+		this.userId = userId;
+		this.properties = properties;
+		this.accessToken = accessToken;
+		this.profiles = profiles;
+		this.selectedProfile = selectedProfile;
+	}
+
+	@Override
+	public String getUserID() {
+		return userId;
+	}
+
+	@Override
+	public UserType getUserType() {
+		return UserType.MOJANG;
+	}
+
+	@Override
+	public Map<String, String> getUserProperties() {
+		return properties;
+	}
+
+	@Override
+	public String getAccessToken() {
+		return accessToken;
+	}
+
+	@Override
+	public GameProfile[] getGameProfiles() {
+		return profiles;
+	}
+
+	@Override
+	public GameProfile getSelectedGameProfile() {
+		return selectedProfile;
+	}
+
+	@Override
+	public String toString() {
+		return "YggdrasilSession [userId=" + userId + ", properties=" + properties + ", accessToken=" + accessToken + ", profiles=" + Arrays.toString(profiles) + ", selectedProfile=" + selectedProfile + "]";
+	}
+
+}
