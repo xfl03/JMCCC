04bf0bce61788a55ac0c44cf031294099b8fa2b9
yushijinhun <yushijinhun@gmail.com>
2016-02-08 12:45:51 +0800

    extract test case super class MinecraftEnvironmentTest

--- src/test/java/org/to2mbn/jmccc/test/AssetTest.java
+++ src/test/java/org/to2mbn/jmccc/test/AssetTest.java
@@ -2,35 +2,23 @@ package org.to2mbn.jmccc.test;
 
 import static org.junit.Assert.*;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.security.GeneralSecurityException;
 import java.util.HashSet;
 import java.util.Set;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-import org.to2mbn.jmccc.option.MinecraftDirectory;
 import org.to2mbn.jmccc.version.Asset;
 import org.to2mbn.jmccc.version.Versions;
 
-public class AssetTest {
+public class AssetTest extends MinecraftEnvironmentTest {
 
-	@Before
-	public void setupMinecraftDir() throws IOException {
-		cleanupMinecraftDir();
+	@Override
+	protected void copyFiles() throws IOException {
 		new File("mcdir/assets/objects/00").mkdirs();
 		copyFromJar("/mcdir/assets/objects/00/000c82756fd54e40cb236199f2b479629d0aca2f", new File("mcdir/assets/objects/00/000c82756fd54e40cb236199f2b479629d0aca2f"));
         copyFromJar("/mcdir/assets/objects/00/000c82756fd54e40cb236199f2b479629d0aca2f", new File("mcdir/assets/objects/00/000c82756fd54e40cb236199f2b479629d0aca20"));
         new File("mcdir/assets/indexes").mkdirs();
         copyFromJar("/mcdir/assets/indexes/test.json", new File("mcdir/assets/indexes/test.json"));
-	}
-
-	@After
-	public void cleanupMinecraftDir() throws IOException {
-		delete(new File("mcdir"));
 	}
 
 	@Test
@@ -57,32 +45,5 @@ public class AssetTest {
         assetsEx.add(new Asset("test/test3.test", "bf7fadaf64945f6b31c803d086ac6a652aabef9b", 3838));
         assertEquals(assetsEx, indexAc);
     }
-
-	private MinecraftDirectory mcdir() {
-		return new MinecraftDirectory("mcdir");
-	}
-
-	private void delete(File file) throws IOException {
-		if (file.exists()) {
-			if (file.isDirectory()) {
-				for (File child : file.listFiles()) {
-					delete(child);
-				}
-			}
-			if (!file.delete()) {
-				throw new IOException("failed to delete: " + file);
-			}
-		}
-	}
-
-	private void copyFromJar(String jarpath, File target) throws IOException {
-		try (InputStream in = getClass().getResourceAsStream(jarpath); OutputStream out = new FileOutputStream(target);) {
-			byte[] buffer = new byte[8192];
-			int read;
-			while ((read = in.read(buffer)) != -1) {
-				out.write(buffer, 0, read);
-			}
-		}
-	}
 
 }
