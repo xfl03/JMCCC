17d3d3ab7dce7e0cdd4ef67a4e93d89602d699a4
yushijinhun <yushijinhun@gmail.com>
2016-02-11 14:57:12 +0800

    add YggdrasilAPIProvider

---
+++ jmccc-yggdrasil-authenticator/src/main/java/org/to2mbn/jmccc/auth/yggdrasil/core/yggdrasil/YggdrasilAPIProvider.java
@@ -0,0 +1,19 @@
+package org.to2mbn.jmccc.auth.yggdrasil.core.yggdrasil;
+
+import java.util.UUID;
+
+public interface YggdrasilAPIProvider {
+
+	String authenticate();
+
+	String refresh();
+
+	String validate();
+
+	String invalidate();
+
+	String signout();
+
+	String profile(UUID profileUUID);
+
+}
