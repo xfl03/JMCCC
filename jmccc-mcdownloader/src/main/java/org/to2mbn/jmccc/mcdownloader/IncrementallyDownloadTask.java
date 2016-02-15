630a55fa49fb0b57797b0513bcf15c3092625dcc
yushijinhun <yushijinhun@gmail.com>
2016-02-15 17:38:44 +0800

    check assets in parallel

--- jmccc-mcdownloader/src/main/java/org/to2mbn/jmccc/mcdownloader/IncrementallyDownloadTask.java
+++ jmccc-mcdownloader/src/main/java/org/to2mbn/jmccc/mcdownloader/IncrementallyDownloadTask.java
@@ -8,6 +8,8 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.security.NoSuchAlgorithmException;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.Callable;
@@ -154,11 +156,24 @@ public class IncrementallyDownloadTask extends CombinedDownloadTask<Version> {
 		}
 	}
 
-	private void downloadAssets(CombinedDownloadContext<Version> context, Set<Asset> assets) throws NoSuchAlgorithmException, IOException, InterruptedException {
+	private void downloadAssets(final CombinedDownloadContext<Version> context, Set<Asset> assets) throws NoSuchAlgorithmException, IOException, InterruptedException {
+		Map<String, Asset> hashMapping = new HashMap<>();
 		for (Asset asset : assets) {
-			if (!asset.isValid(mcdir)) {
-				context.submit(downloadProvider.asset(mcdir, asset), null, false);
-			}
+			// put the assets into a map
+			// to remove the elements which has the same hash
+			hashMapping.put(asset.getHash(), asset);
+		}
+		for (final Asset asset : hashMapping.values()) {
+			context.submit(new Callable<Object>() {
+
+				@Override
+				public Object call() throws Exception {
+					if (!asset.isValid(mcdir)) {
+						context.submit(downloadProvider.asset(mcdir, asset), null, false);
+					}
+					return null;
+				}
+			}, null, false);
 		}
 	}
 
