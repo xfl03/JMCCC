751f9104ce17b49efc83c71c88fafc2d30b865ef
yushijinhun <yushijinhun@gmail.com>
2016-05-07 18:40:46 +0800

    Split YggdrasilServiceBuilder

---
+++ jmccc-yggdrasil-authenticator/src/main/java/org/to2mbn/jmccc/auth/yggdrasil/core/yggdrasil/YggdrasilAuthenticationServiceBuilder.java
@@ -0,0 +1,32 @@
+package org.to2mbn.jmccc.auth.yggdrasil.core.yggdrasil;
+
+import org.to2mbn.jmccc.auth.yggdrasil.core.Agent;
+import org.to2mbn.jmccc.auth.yggdrasil.core.AuthenticationService;
+
+public class YggdrasilAuthenticationServiceBuilder extends AbstractYggdrasilServiceBuilder<AuthenticationService> {
+
+	public static YggdrasilAuthenticationServiceBuilder create() {
+		return new YggdrasilAuthenticationServiceBuilder();
+	}
+
+	public static AuthenticationService buildDefault() {
+		return create().build();
+	}
+
+	protected Agent agent;
+	
+	public YggdrasilAuthenticationServiceBuilder agent(Agent agent) {
+		this.agent = agent;
+		return this;
+	}
+
+	@Override
+	public AuthenticationService build() {
+		return new YggdrasilAuthenticationService(buildJSONHttpRequester(), buildPropertiesDeserializer(), buildAPIProvider(), buildAgent());
+	}
+
+	protected Agent buildAgent() {
+		return agent == null ? Agent.MINECRAFT : agent;
+	}
+
+}
---
+++ jmccc-yggdrasil-authenticator/src/main/java/org/to2mbn/jmccc/auth/yggdrasil/core/yggdrasil/YggdrasilProfileServiceBuilder.java
@@ -0,0 +1,20 @@
+package org.to2mbn.jmccc.auth.yggdrasil.core.yggdrasil;
+
+import org.to2mbn.jmccc.auth.yggdrasil.core.ProfileService;
+
+public class YggdrasilProfileServiceBuilder extends AbstractYggdrasilServiceBuilder<ProfileService> {
+
+	public static YggdrasilProfileServiceBuilder create() {
+		return new YggdrasilProfileServiceBuilder();
+	}
+
+	public static ProfileService buildDefault() {
+		return create().build();
+	}
+
+	@Override
+	public ProfileService build() {
+		return new YggdrasilProfileService(buildJSONHttpRequester(), buildPropertiesDeserializer(), buildAPIProvider());
+	}
+
+}
