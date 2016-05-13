281e8a80987cb55f805fc3628bba5da4852b8a32
yushijinhun <yushijinhun@gmail.com>
2016-05-13 22:29:08 +0800

    remove @Deprecated

--- jmccc-mcdownloader/src/main/java/org/to2mbn/jmccc/mcdownloader/provider/MojangDownloadProvider.java
+++ jmccc-mcdownloader/src/main/java/org/to2mbn/jmccc/mcdownloader/provider/MojangDownloadProvider.java
@@ -2,19 +2,16 @@ package org.to2mbn.jmccc.mcdownloader.provider;
 
 public class MojangDownloadProvider extends DefaultLayoutProvider {
 
-	@Deprecated
 	@Override
 	protected String getLibraryBaseURL() {
 		return "https://libraries.minecraft.net/";
 	}
 
-	@Deprecated
 	@Override
 	protected String getVersionBaseURL() {
 		return "http://s3.amazonaws.com/Minecraft.Download/versions/";
 	}
 
-	@Deprecated
 	@Override
 	protected String getAssetIndexBaseURL() {
 		return "http://s3.amazonaws.com/Minecraft.Download/indexes/";
