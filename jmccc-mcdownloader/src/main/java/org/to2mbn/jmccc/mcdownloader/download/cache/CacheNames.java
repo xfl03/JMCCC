8ac0d829d38117bfaf2edc0a26b708c80583200f
yushijinhun <yushijinhun@gmail.com>
2016-06-17 16:25:13 +0800

    Turn on formatter for CacheNames

--- jmccc-mcdownloader/src/main/java/org/to2mbn/jmccc/mcdownloader/download/cache/CacheNames.java
+++ jmccc-mcdownloader/src/main/java/org/to2mbn/jmccc/mcdownloader/download/cache/CacheNames.java
@@ -2,36 +2,32 @@ package org.to2mbn.jmccc.mcdownloader.download.cache;
 
 public final class CacheNames {
 
-	// @formatter:off
-	
-	private static final String PREFIX 					= "org.to2mbn.jmccc.mcdownloader.cache.";
-	private static final String PREFIX_STATIC 			= PREFIX + "static.";
-	private static final String PREFIX_DYNAMIC 			= PREFIX + "dynamic.";
+	private static final String PREFIX = "org.to2mbn.jmccc.mcdownloader.cache.";
+	private static final String PREFIX_STATIC = PREFIX + "static.";
+	private static final String PREFIX_DYNAMIC = PREFIX + "dynamic.";
 
 	// Default cache
-	public static final String DEFAULT 					= PREFIX + "default";
+	public static final String DEFAULT = PREFIX + "default";
 
 	// Official minecraft caches
-	public static final String VERSION_LIST 			= PREFIX_DYNAMIC + "versionList";
-	public static final String ASSET_INDEX 				= PREFIX_STATIC + "assetsIndex";
-	public static final String GAME_JAR 				= PREFIX_STATIC + "gameJar";
-	public static final String VERSION_JSON 			= PREFIX_DYNAMIC + "versionJson";
-	public static final String LIBRARY 					= PREFIX_STATIC + "library";
-	public static final String ASSET 					= PREFIX_STATIC + "asset";
+	public static final String VERSION_LIST = PREFIX_DYNAMIC + "versionList";
+	public static final String ASSET_INDEX = PREFIX_STATIC + "assetsIndex";
+	public static final String GAME_JAR = PREFIX_STATIC + "gameJar";
+	public static final String VERSION_JSON = PREFIX_DYNAMIC + "versionJson";
+	public static final String LIBRARY = PREFIX_STATIC + "library";
+	public static final String ASSET = PREFIX_STATIC + "asset";
 
 	// Forge caches
-	public static final String FORGE_VERSION_LIST 		= PREFIX_DYNAMIC + "forge.versionList";
-	public static final String FORGE_INSTALLER 			= PREFIX_STATIC + "forge.installer";
-	public static final String FORGE_UNIVERSAL 			= PREFIX_STATIC + "forge.universal";
+	public static final String FORGE_VERSION_LIST = PREFIX_DYNAMIC + "forge.versionList";
+	public static final String FORGE_INSTALLER = PREFIX_STATIC + "forge.installer";
+	public static final String FORGE_UNIVERSAL = PREFIX_STATIC + "forge.universal";
 
 	// Liteloader caches
-	public static final String LITELOADER_VERSION_LIST 	= PREFIX_DYNAMIC + "liteloader.versionList";
-	public static final String LITELOADER_VERSION_JSON 	= PREFIX_DYNAMIC + "liteloader.versionJson";
+	public static final String LITELOADER_VERSION_LIST = PREFIX_DYNAMIC + "liteloader.versionList";
+	public static final String LITELOADER_VERSION_JSON = PREFIX_DYNAMIC + "liteloader.versionJson";
 
 	// Maven
-	public static final String M2_METADATA 				= PREFIX_DYNAMIC + "m2.metadata";
-
-	// @formatter:on
+	public static final String M2_METADATA = PREFIX_DYNAMIC + "m2.metadata";
 
 	private CacheNames() {}
 
