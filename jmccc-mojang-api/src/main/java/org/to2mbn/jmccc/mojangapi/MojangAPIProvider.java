170599108ef511eeb195e992d97901f52114abcc
yushijinhun <yushijinhun@gmail.com>
2016-06-18 13:41:08 +0800

    Add MojangAPIProvider

---
+++ jmccc-mojang-api/src/main/java/org/to2mbn/jmccc/mojangapi/MojangAPIProvider.java
@@ -0,0 +1,18 @@
+package org.to2mbn.jmccc.mojangapi;
+
+import java.util.UUID;
+import org.to2mbn.jmccc.auth.yggdrasil.core.TextureType;
+
+public interface MojangAPIProvider {
+
+	String apiStatus();
+
+	String nameHistory(UUID uuid);
+
+	String texture(UUID uuid, TextureType type);
+
+	String userInfo();
+
+	String blockedServers();
+
+}
