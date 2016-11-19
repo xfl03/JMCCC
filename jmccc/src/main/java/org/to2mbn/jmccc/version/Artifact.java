6de2f0ce82b6ec863551c767a86c261094564890
yushijinhun <yushijinhun@gmail.com>
2016-11-19 20:35:17 +0800

    Extract Artifact

---
+++ jmccc/src/main/java/org/to2mbn/jmccc/version/Artifact.java
@@ -0,0 +1,97 @@
+package org.to2mbn.jmccc.version;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Artifact implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	public static String getArtifactBasePath(String groupId, String artifactId, String version) {
+		return groupId.replace('.', '/') + "/" + artifactId + "/" + version + "/";
+	}
+
+	private String groupId;
+	private String artifactId;
+	private String version;
+	private String classifier;
+	private String type = "jar";
+
+	public Artifact(String groupId, String artifactId, String version) {
+		this(groupId, artifactId, version, null, "jar");
+	}
+
+	public Artifact(String groupId, String artifactId, String version, String classifier, String type) {
+		this.groupId = Objects.requireNonNull(groupId);
+		this.artifactId = Objects.requireNonNull(artifactId);
+		this.version = Objects.requireNonNull(version);
+		this.classifier = classifier;
+		this.type = Objects.requireNonNull(type);
+	}
+
+	public String getGroupId() {
+		return groupId;
+	}
+
+	public String getArtifactId() {
+		return artifactId;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public String getClassifier() {
+		return classifier;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public boolean isSnapshotArtifact() {
+		return version.endsWith("-SNAPSHOT");
+	}
+
+	public String getPath() {
+		return getPath0(version);
+	}
+
+	public String getPath(String snapshotPostfix) {
+		if (!isSnapshotArtifact()) {
+			throw new IllegalArgumentException("The artifact is not a snapshot.");
+		}
+		return getPath0(version.substring(0, version.length() - "SNAPSHOT".length()) + snapshotPostfix);
+	}
+
+	private String getPath0(String version0) {
+		return getArtifactBasePath(groupId, artifactId, version) + artifactId + "-" + version0 + (classifier == null ? "" : "-" + classifier) + "." + type;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == this) {
+			return true;
+		}
+		if (obj instanceof Artifact) {
+			Artifact another = (Artifact) obj;
+			return Objects.equals(groupId, another.groupId)
+					&& Objects.equals(artifactId, another.artifactId)
+					&& Objects.equals(version, another.version)
+					&& Objects.equals(classifier, another.classifier)
+					&& Objects.equals(type, another.type);
+		}
+		return false;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(groupId, artifactId, version, classifier, type);
+	}
+
+	@Override
+	public String toString() {
+		return groupId + ":" + artifactId + ":" + version + (classifier == null ? "" : ":" + classifier);
+	}
+
+}
--- jmccc/src/main/java/org/to2mbn/jmccc/version/Library.java
+++ jmccc/src/main/java/org/to2mbn/jmccc/version/Library.java
@@ -1,23 +1,11 @@
 package org.to2mbn.jmccc.version;
 
-import java.io.Serializable;
 import java.util.Objects;
 import org.to2mbn.jmccc.option.MinecraftDirectory;
 
-public class Library implements Serializable {
+public class Library extends Artifact {
 
 	private static final long serialVersionUID = 1L;
-
-	public static String getArtifactBasePath(String groupId, String artifactId, String version) {
-		return groupId.replace('.', '/') + "/" + artifactId + "/" + version + "/";
-	}
-
-	// Maven standard fields
-	private String groupId;
-	private String artifactId;
-	private String version;
-	private String classifier;
-	private String type = "jar";
 
 	// Minecraft customized fields
 	private LibraryInfo downloadInfo;
@@ -25,7 +13,7 @@ public class Library implements Serializable {
 	private String[] checksums;
 
 	public Library(String groupId, String artifactId, String version) {
-		this(groupId, artifactId, version, null, "jar", null);
+		this(groupId, artifactId, version, null, "jar");
 	}
 
 	public Library(String groupId, String artifactId, String version, String classifier, String type) {
@@ -37,30 +25,22 @@ public class Library implements Serializable {
 	}
 
 	public Library(String groupId, String artifactId, String version, String classifier, String type, LibraryInfo downloadInfo, String customizedUrl, String[] checksums) {
-		this.groupId = Objects.requireNonNull(groupId);
-		this.artifactId = Objects.requireNonNull(artifactId);
-		this.version = Objects.requireNonNull(version);
-		this.classifier = classifier;
-		this.type = Objects.requireNonNull(type);
+		super(groupId, artifactId, version, classifier, type);
 		this.downloadInfo = downloadInfo;
 		this.customizedUrl = customizedUrl;
 		this.checksums = checksums;
 	}
 
-	// Getters
-	// @formatter:off
-	public String getGroupId() { return groupId; }
-	public String getArtifactId() { return artifactId; }
-	public String getVersion() { return version; }
-	public String getClassifier() { return classifier; }
-	public String getType() { return type; }
-	public LibraryInfo getDownloadInfo() { return downloadInfo; }
-	public String getCustomizedUrl() { return customizedUrl; }
-	public String[] getChecksums() { return checksums; }
-	// @formatter:on
+	public LibraryInfo getDownloadInfo() {
+		return downloadInfo;
+	}
 
-	public boolean isSnapshotArtifact() {
-		return version.endsWith("-SNAPSHOT");
+	public String getCustomizedUrl() {
+		return customizedUrl;
+	}
+
+	public String[] getChecksums() {
+		return checksums;
 	}
 
 	/**
@@ -73,21 +53,6 @@ public class Library implements Serializable {
 		return !minecraftDir.getLibrary(this).isFile();
 	}
 
-	public String getPath() {
-		return getPath0(version);
-	}
-
-	public String getPath(String snapshotPostfix) {
-		if (!isSnapshotArtifact()) {
-			throw new IllegalArgumentException("The artifact is not a snapshot.");
-		}
-		return getPath0(version.substring(0, version.length() - "SNAPSHOT".length()) + snapshotPostfix);
-	}
-
-	private String getPath0(String version0) {
-		return getArtifactBasePath(groupId, artifactId, version) + artifactId + "-" + version0 + (classifier == null ? "" : "-" + classifier) + "." + type;
-	}
-
 	@Override
 	public boolean equals(Object obj) {
 		if (obj == this) {
@@ -95,26 +60,12 @@ public class Library implements Serializable {
 		}
 		if (obj instanceof Library) {
 			Library another = (Library) obj;
-			return Objects.equals(groupId, another.groupId)
-					&& Objects.equals(artifactId, another.artifactId)
-					&& Objects.equals(version, another.version)
-					&& Objects.equals(classifier, another.classifier)
-					&& Objects.equals(type, another.type)
+			return super.equals(obj)
 					&& Objects.equals(downloadInfo, another.downloadInfo)
 					&& Objects.equals(customizedUrl, another.customizedUrl)
 					&& Objects.deepEquals(checksums, another.checksums);
 		}
 		return false;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(groupId, artifactId, version, classifier, type);
-	}
-
-	@Override
-	public String toString() {
-		return groupId + ":" + artifactId + ":" + version + (classifier == null ? "" : ":" + classifier);
 	}
 
 }
